b342f6d38bc1f555398e5cf858435442167cf7fd
juan camilo castillo saucedo <jcgato93@hotmail.com>
2018-07-10 18:41:54 -0500

    Scrapping download image

--- webScrapping.rb
+++ webScrapping.rb
@@ -33,3 +33,38 @@ while moreEntries do
    		url = document.css('div.nav-entries > div.nav-next > a').attr('href')
    	end
 end
+
+
+
+#=========================
+# Descargando imagenes
+#=========================
+
+require'nokogiri'
+require'open-uri'
+
+url = 'http://www.reactiongifs.com/page/'
+document = nil
+contador = 0
+
+begin
+    puts "Pagina: #{contador}"
+
+    begin
+    document = Nokogiri::HTML(open(url + contador.to_s + '/'))
+    rescueOpenURI::HTTPError => e
+        document = nil
+    end
+    div_main = document.css('div#main')
+    div_main.css('div.post').each do |post|
+      title = post.css('h2').text
+      gif_url = post.css('div.entry img').attr('src')
+      File.open("gifs/#{title}.gif", 'w') do |new_file|
+        puts "Descargando: #{title}"
+        open(gif_url, 'r') do |gif|
+            new_file.write(gif.read)
+        end
+      end  
+    end
+    contador+=1
+end until document.nil?
